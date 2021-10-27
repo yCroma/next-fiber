fcf14c75f1d28afa2e3c65ea3b2a497c8eb0c776
yCroma <kuroyuki0728@gmail.com>
2021-10-27 17:05:41 +0900

    ADD: three.jsでアニメーションをするコンポーネントを追加
    mutableなレンダラーを実現するために、
    生のthree.jsを利用する必要が出てきた。
    素振りのコンポーネントを作成した。

---
+++ components/FBXPlayer.tsx
@@ -0,0 +1,54 @@
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
+
+const FBXPlayer = ({ url, preset }) => {
+  const canvasRef = useRef(null);
+  console.log(preset);
+
+  useEffect(() => {
+    console.log(canvasRef.current);
+    const canvas = canvasRef.current;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+
+    const scene = new THREE.Scene();
+
+    // カメラを作成
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.set(0, 0, +1000);
+    scene.add(camera);
+
+    // light
+    const AmbientLight = new THREE.AmbientLight(0xffffff, 4.0);
+    scene.add(AmbientLight);
+
+    // 箱を作成
+    const geometry = new THREE.BoxGeometry(400, 400, 400);
+    const material = new THREE.MeshNormalMaterial();
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // animate
+    const animate = () => {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
+    animate();
+  });
+
+  return (
+    <>
+      <h2>This is FBXPlayer</h2>
+      <h3>{url}</h3>
+      <canvas ref={canvasRef}></canvas>
+    </>
+  );
+};
+
+export default FBXPlayer;
