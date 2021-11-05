9983f90b81e94fc2cbf685f2e5352e51138cde3f
yCroma <kuroyuki0728@gmail.com>
2021-11-06 02:46:08 +0900

    ADD: playerページの再生DOMのサイズを設定
    FBXPlayerは、100%のサイズで表示される。
    よって、親側のエレメントからサイズを指定
    するようにした。

--- pages/player.tsx
+++ pages/player.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import FBXPlayer from "../components/FBXPlayer";
 
+import { Box } from "@chakra-ui/react";
+
 const Player: NextPage = () => {
   const fbxurl = "/test.fbx";
   const PresetInit = {
@@ -11,7 +13,9 @@ const Player: NextPage = () => {
   return (
     <>
       <h1>hello</h1>
-      <FBXPlayer url={fbxurl} preset={preset} />
+      <Box w="80%" m="auto">
+        <FBXPlayer url={fbxurl} preset={preset} />
+      </Box>
     </>
   );
 };
