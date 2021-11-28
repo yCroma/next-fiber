bb4e2978ef862d6a62de62e1d3eddc9719d777ba
yCroma <kuroyuki0728@gmail.com>
2021-11-28 23:29:32 +0900

    UPDATE: playの型をbooleanにした
    Booleanで型エラーが起こっていた。
    適切なのがbooleanだったので割り当てた

--- src/components/FBXPlayer.tsx
+++ src/components/FBXPlayer.tsx
@@ -19,17 +19,17 @@ interface Time {
 
 const FBXPlayer = ({ url, preset }: { url: string; preset: object }) => {
   const [time, setTime] = useState<DOMHighResTimeStamp>(0);
-  const [play, setPlay] = useState<Boolean>(false);
+  const [play, setPlay] = useState<boolean>(false);
   const [action, setAction] = useState<THREE.AnimationAction>();
 
   return (
     <>
       <h2>This is FBXPlayer</h2>
       <h3>{url}</h3>
-      <Renderer url={url} setTime={setTime} />
       <h4>{time}</h4>
-        <PlayBackButton setPlay={setPlay} play={play} />
+      <Renderer url={url} play={play} setTime={setTime} setPlay={setPlay} />
       <ScrubThrough time={time}></ScrubThrough>
+        <PlayBackButton onClick={() => setPlay(!play)} play={play} />
     </>
   );
 };
