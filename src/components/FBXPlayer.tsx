18d3e062318da131ac4e59562a2bd1f554a89a42
yCroma <kuroyuki0728@gmail.com>
2021-11-28 14:24:56 +0900

    UPDATE: Rendererのコンポーネントを作成して分離した
    既存のFBXPlayerでアニメーションを行うと、
    親コンポーネントの更新とみなされてしまう。
    Hooksを使っていくうえで都合が悪いので、
    アニメーションに特化したコンポーネントを作成
    して分離。

--- src/components/FBXPlayer.tsx
+++ src/components/FBXPlayer.tsx
@@ -5,6 +5,7 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import useAnimation from "../customhooks/useAnimation";
 
 import { chakra } from "@chakra-ui/react";
+import Renderer from "./blocks/Renderer";
 import PlaybackBar from "./PlaybackBar";
 
 interface props {
@@ -41,86 +42,15 @@ const Canvas = chakra("canvas");
 
 const FBXPlayer = ({ url, preset }: props) => {
   const [time, setTime] = useState(0);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const threeRef = useRef<ThreeParams>({});
-  const modelRef = useRef<model>({});
   //const timeRef = useRef<Time>({});
   const [action, setAction] = useState<THREE.AnimationAction>();
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const store = threeRef.current;
-    // renderer
-    store.renderer = new THREE.WebGLRenderer({
-      canvas: canvas!,
-    });
-    // scene
-    store.scene = new THREE.Scene();
-    // camera
-    store.camera = new THREE.PerspectiveCamera(
-      45,
-      canvas!.clientWidth / canvas!.clientHeight
-    );
-    store.camera.position.set(0, 0, +100);
-    store.scene.add(store.camera);
-    // controls
-    const controls = new OrbitControls(store.camera, canvas!);
-    // light
-    const AmbientLight = new THREE.AmbientLight(0xffffff, 4.0);
-    store.scene.add(AmbientLight);
-
-    // 3Dモデルをロード
-    const loader = new FBXLoader();
-    loader.load(url, (model: THREE.Group) => {
-      modelRef.current.model = model;
-      modelRef.current.mixer = new THREE.AnimationMixer(model);
-      modelRef.current.animations = model.animations;
-      // 今回の開発では、1番目に登録されているアニメーションのみに対応する
-      modelRef.current.actions = [
-        modelRef.current.mixer.clipAction(modelRef.current.animations[0]),
-      ];
-      modelRef.current.actions[0].play();
-      store.scene!.add(model);
-      // モデルを読み込んだ際に時間関連のデータも初期化
-      // timeRef.current.time = 0;
-      // timeRef.current.step = 0.001;
-      // timeRef.current.start = 0;
-      // timeRef.current.end = modelRef.current.animations[0].duration;
-    });
-  });
-
-  const resizehandle = () => {
-    threeRef.current.renderer!.setSize(
-      canvasRef.current!.clientWidth,
-      canvasRef.current!.clientHeight,
-      false
-    );
-    threeRef.current.camera!.aspect =
-      canvasRef.current!.clientWidth / canvasRef.current!.clientHeight;
-    threeRef.current.camera!.updateProjectionMatrix;
-  };
-
-  const animate = (deltaTime: DOMHighResTimeStamp) => {
-    resizehandle();
-    if (modelRef.current.mixer) {
-      modelRef.current.mixer.update(deltaTime);
-      //console.log(modelRef.current.actions![0].time);
-      //setTime(modelRef.current.actions![0].time);
-    }
-    threeRef.current.renderer!.render(
-      threeRef.current.scene!,
-      threeRef.current.camera!
-    );
-  };
-
-  useAnimation(animate);
 
   return (
     <>
       <h2>This is FBXPlayer</h2>
       <h3>{url}</h3>
-      <Canvas w="100%" ref={canvasRef}></Canvas>
       <PlaybackBar time={() => modelRef.current}></PlaybackBar>
+      <Renderer url={url} />
       <h4>{time}</h4>
     </>
   );
---
+++ src/components/blocks/Renderer.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import useAnimation from "../../customhooks/useAnimation";
+
+import { chakra } from "@chakra-ui/react";
+
+interface props {
+  url: string;
+}
+
+interface model {
+  model?: THREE.Group;
+  mixer?: THREE.AnimationMixer;
+  animations?: Array<THREE.AnimationClip>;
+  actions?: Array<THREE.AnimationAction>;
+}
+
+interface ThreeParams {
+  renderer?: THREE.Renderer;
+  camera?: THREE.PerspectiveCamera;
+  scene?: THREE.Scene;
+}
+
+const Canvas = chakra("canvas");
+
+const Renderer = ({ url }: props) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const threeRef = useRef<ThreeParams>({});
+  const modelRef = useRef<model>({});
+  useEffect(() => {
+    const canvas = canvasRef.current || document.createElement("canvas");
+    const store = threeRef.current;
+    // renderer
+    store.renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    // screen
+    store.scene = new THREE.Scene();
+    // camera
+    store.camera = new THREE.PerspectiveCamera(
+      45,
+      canvas!.clientWidth / canvas!.clientHeight
+    );
+    store.camera.position.set(0, 0, +100);
+    store.scene.add(store.camera);
+    // controls
+    const controls = new OrbitControls(store.camera, canvas!);
+    // light
+    const AmbientLight = new THREE.AmbientLight(0xffffff, 4.0);
+    store.scene.add(AmbientLight);
+    // 3Dモデルをロード
+    const loader = new FBXLoader();
+    loader.load(url, (model: THREE.Group) => {
+      modelRef.current.model = model;
+      modelRef.current.mixer = new THREE.AnimationMixer(model);
+      modelRef.current.animations = model.animations;
+      // 今回の開発では、1番目に登録されているアニメーションのみに対応する
+      modelRef.current.actions = [
+        modelRef.current.mixer.clipAction(modelRef.current.animations[0]),
+      ];
+      modelRef.current.actions[0].play();
+      store.scene!.add(model);
+      // モデルを読み込んだ際に時間関連のデータも初期化
+      // timeRef.current.time = 0;
+      // timeRef.current.step = 0.001;
+      // timeRef.current.start = 0;
+      // timeRef.current.end = modelRef.current.animations[0].duration;
+    });
+  }, []);
+  // 解像度用のresize
+  const resizehandle = () => {
+    threeRef.current.renderer!.setSize(
+      canvasRef.current!.clientWidth,
+      canvasRef.current!.clientHeight,
+      false
+    );
+    threeRef.current.camera!.aspect =
+      canvasRef.current!.clientWidth / canvasRef.current!.clientHeight;
+    threeRef.current.camera!.updateProjectionMatrix;
+  };
+  const animate = (deltaTime: DOMHighResTimeStamp) => {
+    resizehandle();
+    if (modelRef.current.mixer) {
+      modelRef.current.mixer.update(deltaTime);
+      //console.log(modelRef.current.actions![0].time);
+      //setTime(modelRef.current.actions![0].time);
+    }
+    threeRef.current.renderer!.render(
+      threeRef.current.scene!,
+      threeRef.current.camera!
+    );
+  };
+
+  useAnimation(animate);
+  return (
+    <>
+      <Canvas w="100%" ref={canvasRef}></Canvas>
+    </>
+  );
+};
+
+export default Renderer;
