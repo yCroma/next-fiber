4b96e20392442854c476bee4658a526fbd5d8be5
yCroma <kuroyuki0728@gmail.com>
2021-12-10 15:42:22 +0900

    ADD: upload用のページを追加

---
+++ web/src/pages/upload.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Grid, Typography } from "@mui/material";
+
+const Upload: NextPage = () => {
+  return (
+    <Grid container spacing={0}>
+      <Typography variant="h2">Upload a FBX file</Typography>
+    </Grid>
+  );
+};
+
+export default Upload;
