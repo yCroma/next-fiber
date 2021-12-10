a40386fa87424745df085a382f7a352f241c399c
yCroma <kuroyuki0728@gmail.com>
2021-12-10 13:48:54 +0900

    ADD: dbと連携する実装を追加

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,8 +4,21 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
