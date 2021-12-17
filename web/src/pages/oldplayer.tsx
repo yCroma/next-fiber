82d79d8a41abd9cfb916824030236d2e6df4bcc2
yCroma <kuroyuki0728@gmail.com>
2021-12-17 19:59:11 +0900

    ADD: oldplayer.tsx
    既存で開発していたプレイヤーが
    不要になったため、名前を更新
    新しいプレイヤーが完全に完成する
    までは残しておく。

---
+++ web/src/pages/oldplayer.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import FBXPlayer from "../ui/units/FBXPlayer";
+import NewPlayer from "../ui/units/NewPlayer";
+
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+
+const Player: NextPage = () => {
+  const fbxurl = "/test.fbx";
+  const PresetInit = {
+    name: "hello, fbx",
+  };
+  const [preset, SetPreset] = useState(PresetInit);
+  return (
+    <Stack sx={{ width: "80%", mx: "auto" }}>
+      <NewPlayer url={fbxurl} />
+      <Stack>
+        <FBXPlayer url={fbxurl} preset={preset} />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Player;
