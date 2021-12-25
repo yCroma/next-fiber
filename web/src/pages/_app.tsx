c49a2dfe32c36e2e7ad10bdf0e9ebdb128ab4970
yCroma <58209438+yCroma@users.noreply.github.com>
2021-12-25 22:22:40 +0900

    ADD: headにviewportを追加

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import "../../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
