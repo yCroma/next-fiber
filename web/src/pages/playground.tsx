f8f8d2aec002cb3abbce0f15661b46482df2dade
yCroma <58209438+yCroma@users.noreply.github.com>
2022-01-08 10:09:26 +0900

    ADD: レンダラーのモードを管理するhooksの追加
    レンダラーとページの結合度を強くしたく
    なかったので、useReducerを利用してイベント
    にすることにした。

--- web/src/pages/playground.tsx
+++ web/src/pages/playground.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next';
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
@@ -20,6 +20,22 @@ const Player: NextPage<{ title: string; comment: string; fbxurl: string }> = ({
     name: 'hello, fbx',
   };
   const [preset, SetPreset] = useState(PresetInit);
+
+  const initialMode = { mode: 'edit' };
+  function modeReducer(state, action) {
+    switch (action.type) {
+      case 'edit':
+        return { mode: 'edit' };
+      case 'play':
+        return { mode: 'play' };
+      case 'upload':
+        return { mode: 'upload' };
+      default:
+        throw new Error();
+    }
+  }
+  const [mode, dispatchMode] = useReducer(modeReducer, initialMode);
+
   return (
     <Stack sx={{ width: '80%', mx: 'auto' }}>
       <Typography variant="h3">{title}</Typography>
