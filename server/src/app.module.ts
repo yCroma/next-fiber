29b4de235cd07e74b0e24846e9dd20648f83fdb5
yCroma <58209438+yCroma@users.noreply.github.com>
2022-01-26 22:58:01 +0900

    UPDATE: app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UploadModule } from './upload/upload.module';
+import { Asset } from './upload/asset.entity'
+import { WorksController } from './works/works.controller';
+import { WorksService } from './works/works.service';
+import { WorksModule } from './works/works.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": "db",
+    "port": 3306,
+    "username": "root",
+    "password": "root",
+    "database": "develop",
+    "entities": [Asset],
+    "synchronize": true,
+    "autoLoadEntities": true,
+  }), UploadModule, WorksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
